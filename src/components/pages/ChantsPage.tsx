feb30e19c8eed1673ed9561bf6c66b6774ff0c7e
Wix Remote Machine <support@wix.com>
2026-02-02 13:06:27 +0000

    Manual code changes

--- src/components/pages/ChantsPage.tsx
+++ src/components/pages/ChantsPage.tsx
@@ -315,7 +315,7 @@ export default function ChantsPage() {
                     <h3 className="text-xs tracking-widest uppercase text-stone-500 mb-4">
                       Listen
                     </h3>
-                    <ModernAudioPlayer 
+                    
                       audioUrl={selectedChant.audio}
                       title={selectedChant.chantTitle || 'Sacred Chant'}
                     />
